2ce71cd9a89f2b162dc7c6f472018e4049e94894
Salomon Thomas <salomon.websight@gmail.com>
2023-09-01 19:41:00 +0100

    port update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`API listening on port ${port}`);
 });
